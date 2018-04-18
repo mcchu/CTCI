c66c3a607b8d01896b17896b01de86760456c71b
Michael Chu <chumc@usc.edu>
2018-04-18 15:20:29 -0700

    Adding GraphBFS and etc algorithms
    GraphBFS, Isolation within Array, and AutoComplete algorithms

---
+++ bst.h
@@ -0,0 +1,72 @@
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+
+//------ BST Implementation ------
+//
+//----------------------------------------
+
+struct leaf
+{
+    int val;
+    struct leaf *left;
+    struct leaf *right;
+};
+
+class BST
+{
+	leaf* root;
+	
+	public:
+		BST(){
+			root = NULL;
+		}
+				
+		void insert(leaf* node, leaf* newnode){
+			if(root == NULL){
+				root = new leaf;
+				root->val = newnode->val;
+				root->left = NULL;
+				root->right = NULL;
+				std::cout << "Root Node is Added" << std::endl;
+				return;
+			}	
+			if(node->val > newnode->val){	//Newnode goes to the left
+				if(node->left == NULL){
+					newnode->left = NULL;
+					newnode->right = NULL;
+					node->left = newnode;
+					std::cout << "Node added to left." << std::endl;
+					return;
+				}
+				else{
+					insert(node->left, newnode);
+				}
+			}
+			else if(node->val < newnode->val){	//Newnode goes to the right
+				if(node->right == NULL){
+					newnode->left == NULL;
+					newnode->right == NULL;
+					node->right = newnode;
+					std::cout << "Node added to right." << std::endl;
+					return;
+				}
+				else{
+					insert(node->right,newnode);
+				}
+			}
+			else{
+				std::cout << "Node value already in tree." << std::endl;
+				return;
+			}			
+		}
+		
+		void del(int val);
+		void print(leaf* node);
+		void preorder(leaf* node);
+		void inorder(leaf* node);
+		void postorder(leaf* node);	
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 //Test file for cracking the code interview programming questions
 #include <string>
+#include <vector>
 #include <functional>
+#include <unordered_map>
 #include "hash_table.h"
 #include "linked_list.h"
 #include "stack.h"
 #include "queue.h"
+#include "bst.h"
 
 using namespace std;
 
@@ -12,12 +15,110 @@ void implementHashTable();
 void implementLinkedList();
 void implementStack();
 void implementQueue();
+void implementGraphBFS();
+
+//Graph BFS Functions
+void dfs(vector<vector<int>> &graph, vector<vector<int>> &output, vector<int> path, int start, int end);
+vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph);
+void printpath(vector<vector<int>> path);
+
+
+//Algorithm: Find and output the only number that appears once in the array
+vector<int> DistinctValues(vector<vector<int>> values)
+{
+	vector<int> output;
+	
+	for(vector<int> vec_val : values){
+		unordered_map<int,int> values_map;		
+		for(int val : vec_val){
+			values_map[val]++;
+		}
+		for(unordered_map<int,int>::iterator it = values_map.begin(); it != values_map.end(); it++){
+			if(it->second == 1){
+				output.push_back(it->first);
+			}
+		}
+	}
+	return output;
+}
+
+//Algorithm: Find and output the values that we can autocomplete
+vector<string> AutoComplete(vector<string> dict, string key)
+{
+	vector<string> completed;
+	
+	//First we need to parse the key into workable parts. 
+	vector<string> key_subs;
+	int st_idx = 0, idx = 0, len = 1;
+		
+	while(idx <= key.size()){
+		if(isupper(key[idx])){
+			st_idx = idx;
+			idx++;
+			while(islower(key[idx])){
+				idx++;
+				len++;
+			}
+			string subs = key.substr(st_idx,len);
+			key_subs.push_back(subs);		
+		}
+		else{
+			idx++;
+		}
+		len = 1;
+	}
+	
+	//Then we iterate through the dictionary to output our string
+	int count = 0;												
+	for(string str : dict){										//Loop through Dictionary First
+		int i = 0;
+		for(string ss : key_subs){								//Loop through Key Vectors
+			for(; i < str.size(); i++){				//Loop through String
+				if(str.substr(i).find(ss) != string::npos){
+					i += str.substr(i).find(ss);
+					count += 1;
+				}
+				else
+					break;
+			}
+		}
+		if(count == key_subs.size()){
+			completed.push_back(str);
+		}
+		count = 0;		
+	}
+	
+	return completed;
+}
 
 int main()
 {
-	int choice;
 
-	while(1)
+	//Find and output the only number that appears once in the array
+	//Sample Input: [2,2,2,6],[1,2,1,1,1,1]
+	//Sample Output: [6,2]
+	vector<vector<int>> values = {{2,2,2,6},{1,2,1,1,1,1,},{3,3,7},{9,9,9,10,9,9,9,9,9,9,9,9}};
+	vector<int> output = DistinctValues(values);
+	cout << "== Outputting the number that apperas only once ==\n";
+	for(int i : output){
+		cout << i << endl;
+	}
+	cout << endl;
+	
+	//Given a string, autocomplete the string depending on the given input_iterator
+	//Sample Input: ["Data", "GVi", DaGVi","DataHandler"]
+	//Sample Output: ["DataGraphView","DataGraphHandler","DataGraphVisual"]["GraphViewData","DataGraphVisual"]["DataGraphView"]["DataGraphHandler"]
+	vector<string> s_values = {"DataGraphView", "DataGraphHandler", "DataGraphVisual", "GraphPartyOut"};
+	string input = "DG";
+	vector<string> s_output = AutoComplete(s_values, input);
+	cout << "== Outputting the AutoCompleted string ==\n";
+	for(string s : s_output){
+		cout << s << endl;
+	}
+	
+	
+	int choice;
+	while(0)
 	{
         cout<<"\n*************************************"<<endl;
         cout<<"Data Structure Implementation Examples"<<endl;
@@ -26,7 +127,8 @@ int main()
 		cout<<"2. Linked List"<<endl;
 		cout<<"3. Stack"<<endl;
 		cout<<"4. Queue"<<endl;
-        cout<<"5. Exit"<<endl;	
+		cout<<"5. Graph"<<endl;
+        cout<<"6. Exit"<<endl;	
         cout<<"Enter your choice: ";
         cin >> choice;
 		cout<< endl;
@@ -44,8 +146,11 @@ int main()
 				implementQueue();
 				break;
 			case 5:
-				exit(1);
+				implementGraphBFS();
 				break;
+			case 6:
+				exit(1);
+				break;				
 			default:
 				cout << "\nERROR: Please enter  valid Selection\n";
        }			
@@ -195,5 +300,56 @@ void implementQueue()
     queue.pop();
 	
 	cout << "End of Test" << endl;	
+}
+
+void implementGraphBFS()
+{
+	//Utilizes a vector<vector<int>> to demonstrate graphdefaults
+	//e.g. 
+	// [[1,2], [3], [3], []]
+	// 0 --> 1
+	// |	 |
+	// v	 v
+	// 2 --> 3
+	vector<vector<int>> graph;
+	graph.resize(4);
+	graph[0].push_back(1);
+	graph[0].push_back(2);
+	graph[1].push_back(3);
+	graph[2].push_back(3);
+	
+	printpath(allPathsSourceTarget(graph));
+	cout << "End of Test" << endl;		
+}
+
+void printpath(vector<vector<int>> path)
+{
+    for (int i = 0; i < path.size(); i++) {
+		for(int j = 0; j < path[i].size(); j++){
+			cout << path[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void dfs(vector<vector<int>> &graph, vector<vector<int>> &output, vector<int> path, int start, int end){
+    path.push_back(start);
+    if(start == end){
+        output.push_back(path);
+        return;
+    }
+    for(auto node : graph[start]){
+		cout << node;
+        dfs(graph, output, path, node, end);
+	}
+}
+
+vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    vector<vector<int>> all_paths;
+    vector<int> path;
+    int nodes = graph.size();
+    if(nodes == 0) return all_paths; //If empty graph
+    dfs(graph, all_paths, path, 0, nodes-1);
+    return all_paths;
 }
 
